53b1e845ab8bce7d12b3e14f9601f02bb84d1541
haanhtun1410 <vunblph27074@fpt.edu.vn>
2024-11-27 03:04:11 +0700

    fix: add vercel config for deployment-a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import vercel  from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,9 +13,5 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: { port: Number(process.env.PORT) || 4321 },
+  adapter: vercel(),
 });
